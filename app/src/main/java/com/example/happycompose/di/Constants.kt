b00b998f8d9f1673e457449ed1bc2a327d98eec6
mehdi <m.taghdisi@digikala.com>
2023-11-29 13:44:44 +0330

    ...

--- app/src/main/java/com/example/happycompose/di/Constants.kt
+++ app/src/main/java/com/example/happycompose/di/Constants.kt
@@ -1,7 +1,5 @@
 package com.example.happycompose.di
 
 object Constants {
-//    const val BASE_URL = "http://172.20.40.88:3002/"
-//    const val BASE_URL = "http://192.168.1.106:3002/"
-    const val BASE_URL = "Your Ip to your server"
+    const val BASE_URL = "http://Your_Ip_to_your_server:portnumber/"
 }
